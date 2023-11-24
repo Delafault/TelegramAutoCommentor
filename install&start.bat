d158560d078ba6c4e539507e31794cbf382f955c
DeFault <78411508+delefault@users.noreply.github.com>
2023-11-24 15:25:25 +0000

    Update install&start.bat

--- install&start.bat
+++ install&start.bat
@@ -11,6 +11,11 @@ if errorlevel 1 (
     pip install tgcrypto
 )
 
+pip show openai > nul
+if errorlevel 1 (
+    pip install openai
+)
+
 echo Запускаю скрипт...
 timeout /t 3 >nul
 start python main.py
